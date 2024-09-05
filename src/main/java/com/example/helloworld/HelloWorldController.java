3d7d0f40c0bbd07d94e5c38fd6cffe013b01a0e9
Illia <kroshka.illya@lll.kpi.ua>
2024-09-05 21:31:43 +0300

    feature: implemented controller

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/sayHello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
