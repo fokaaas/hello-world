8f5157ed29c9c5b428a569fd00c02af6bc61762f
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-09-05 16:29:33 +0300

    feat: added run method with logging

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,13 +1,20 @@
 package com.example.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        System.out.println("Begin of main");
         SpringApplication.run(HelloWorldApplication.class, args);
+        System.out.println("End of main");
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World from Spring Boot!");
+    }
 }
