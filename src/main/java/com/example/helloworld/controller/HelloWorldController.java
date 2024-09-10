7a9e15d75fac8ff311fbfaafd0a0c467f87fa89f
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-09-10 15:56:53 +0300

    enhancement: added devTools

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package com.example.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/sayHello")
+    @GetMapping("/")
     @ResponseBody
     public String sayHello() {
         return "Hello, World!";
